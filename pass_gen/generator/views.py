84ce8f1524f226214614ccb8c0b7f71b2fa3ed9d
js-codebank <jacob.shedd@gmail.com>
2022-11-16 11:12:42 -0500

    gen files

---
+++ pass_gen/generator/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+def home(request):
+    return render(request, 'generator/home.html',{'password':'cheese'})
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+def password(request):
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    upChars = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    nums = list('1234567890')
+    special = list('!@#$%^&*?-+=')
+
+    if request.GET.get('uppercase'):
+        characters+=upChars
+    if request.GET.get('numbers'):
+        characters+=nums
+    if request.GET.get('special'):
+        characters+=special
+    userLength = int(request.GET.get('length',12))
+    if 6 <= userLength <= 14:
+        length = userLength
+    else:
+        length = 12
+
+    thepassword = ''
+    for x in range(length):
+        thepassword+= random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password':thepassword})
